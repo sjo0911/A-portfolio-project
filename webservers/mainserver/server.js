15dcf6b30ab4decbdfdf7fd99a074e10ce93faa6
Simon <jonasson_simon@hotmail.com>
2021-09-01 19:43:11 +0200

    Update server.js

--- webservers/mainserver/server.js
+++ webservers/mainserver/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 
 app.use(express.static('./dist/angularMain'));
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile('index.html', {root: 'dist/angularMain'});
 });
 
